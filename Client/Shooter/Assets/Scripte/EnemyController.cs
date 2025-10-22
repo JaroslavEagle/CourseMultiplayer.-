653d92dd49efe0d4d19080d831a61eca238be56f
JaroslavEagle <yari-o@yandex.ru>
2025-10-22 09:55:23 +0500

    init

---
+++ Client/Shooter/Assets/Scripte/EnemyController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Colyseus.Schema;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    internal void OnChange(List<DataChange> changes)
+    {
+        Vector3 position = transform.position;
+
+        foreach (var dataChange in changes)
+        {
+            switch (dataChange.Field)
+            {
+                case "x":
+                    position.x = (float)dataChange.Value;
+                    break;
+                case "y":
+                    position.z = (float)dataChange.Value;
+                    break;
+                default:
+                    Debug.LogWarning("Не юбрабатываеться изминение поля " + dataChange.Field);
+                    break;
+            }
+        }
+
+        transform.position = position;
+    }
+}
---
+++ Client/Shooter/Assets/Scripte/PlayerCharacter.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2f;
+
+    private float _inputH;
+    private float _inputV;
+
+    public void SetInput(float h,float v)
+    {
+        _inputH = h;
+        _inputV = v;
+    }
+
+    void Update()
+    {
+        Moov();
+    }
+
+    private void Moov()
+    {
+        Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
+
+        transform.position += direction * Time.deltaTime * _speed;
+    }
+
+    public void GetMoveInfo(out Vector3 position)
+    {
+        position = transform.position;
+    }
+}
